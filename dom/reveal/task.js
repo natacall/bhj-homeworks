edb107092ba47de9ed7e8878e3bc8fcd7a29a338
Natali <shatabalova2011@gmail.com>
2022-11-18 21:14:13 +0700

    fixed homework

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -4,9 +4,10 @@ document.addEventListener('scroll', () => {
   const elem = document.querySelectorAll('.reveal');
   elem.forEach(el => {
     const { top, bottom } = el.getBoundingClientRect();
-    if ((top < window.innerHeight) || (bottom > 0)) {
-      el.classList.toggle('reveal_active')
-    }
+    if ((top > 0) && (bottom < window.innerHeight)) {
+      el.classList.add('reveal_active')
+    } else
+      el.classList.remove('reveal_active')
     return false
   });
 })
